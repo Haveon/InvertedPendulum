6da21eee84edb83fea050ba753bac758107aed80
reevesAstronomy <reevesAstronomy@gmail.com>
2016-09-17 18:20:44 -0400

    Attempts at improving the learning ability of the ODE solving NN.

--- Network.py
+++ Network.py
@@ -111,7 +111,7 @@ class Network:
 
         for i in np.arange(iterations):
             # i = iteration
-            print i
+            #print i
             for pos in np.arange(len(labels)): #pos is the index of the training data point being classified.
                 self.Classify(input_vectors[pos])
                 output_deltas = self.getOutputDeltas(labels[pos])
@@ -124,7 +124,7 @@ class Network:
 
         for i in np.arange(iterations):
             # i = iteration
-            print i
+            #print i
 
             #randomize the ordering of the training data
             input_vectors, labels = shuffle_in_unison(input_vectors, labels)
--- test_simple_ODE_solving.py
+++ test_simple_ODE_solving.py
@@ -4,11 +4,11 @@ from Network import Network
 from trainSystem import *
 
 #-------------------------------------------------------------------------------
-#Set everything up before training
+#Set up training data
 
 DEnet = Network(3, 10, 1)
 
-inputs = generateInputs(1e3)
+inputs = generateInputs(1e5)
 outputs= generateOutputs(inputs)
 
 meanTheta = meanUniformDistribution(0, 2*np.pi)
@@ -25,30 +25,54 @@ normedOutputs = (outputs - meanTheta)/stdTheta
 pre_training_error_training_data = classificationError(DEnet, normedInputs, normedOutputs)*100
 
 #-------------------------------------------------------------------------------
-#Training time
-DEnet.Learn(normedInputs, normedOutputs, 10, learningrate=0.01)
+#Set up evaluation data for checking the performance of the trained NN
+
+evaluation_inputs = generateInputs(1e4)
+evaluation_outputs= generateOutputs(evaluation_inputs)
+
+evaluation_meanTheta = meanUniformDistribution(0, 2*np.pi)
+evaluation_meanControl= meanUniformDistribution(-1, 1)
+evaluation_means = np.array([evaluation_meanTheta,evaluation_meanTheta,evaluation_meanControl])
+
+evaluation_stdTheta = stdUniformDistribution(0, 2*np.pi)
+evaluation_stdControl = stdUniformDistribution(0, 2*np.pi)
+evaluation_stds = np.array([evaluation_stdTheta,evaluation_stdTheta,evaluation_stdControl])
+
+evaluation_normedInputs = (evaluation_inputs - evaluation_means)/evaluation_stds
+evaluation_normedOutputs = (evaluation_outputs - evaluation_meanTheta)/evaluation_stdTheta
+
+#-------------------------------------------------------------------------------
+#Training / learning
+
+#DEnet.Learn(normedInputs, normedOutputs, 10, learningrate=0.01)
+
+iterations = 13
+training_data_errors = []
+evaluation_data_errors = []
+learningrates = [1E-2, 1E-2, 5E-3, 1E-3, 5E-4, 1E-4, 5E-5, 5E-5, 1E-5, 1E-5, 1E-5, 1E-5, 1E-5]
+
+for i in xrange(iterations):
+    learningrate_tmp = learningrates[i]
+    DEnet.batchLearn(normedInputs, normedOutputs, 1, learningrate=learningrate_tmp,batch_size=1)
+    print "*Iteration ", i, " classification errors:"
+    training_data_errors.append(classificationError(DEnet, normedInputs, normedOutputs)*100)
+    print "-Training data:", training_data_errors[i], '%'
+    evaluation_data_errors.append(classificationError(DEnet, evaluation_normedInputs, evaluation_normedOutputs)*100)
+    print "-Evaluation data:", evaluation_data_errors[i], '%'
 
 post_training_error_training_data = classificationError(DEnet, normedInputs, normedOutputs)*100
 
 #-------------------------------------------------------------------------------
-#Set up evaluation data and check the performance of the trained NN
+#Output results
 
-inputs = generateInputs(1e3)
-outputs= generateOutputs(inputs)
-
-meanTheta = meanUniformDistribution(0, 2*np.pi)
-meanControl= meanUniformDistribution(-1, 1)
-means = np.array([meanTheta,meanTheta,meanControl])
-
-stdTheta = stdUniformDistribution(0, 2*np.pi)
-stdControl = stdUniformDistribution(0, 2*np.pi)
-stds = np.array([stdTheta,stdTheta,stdControl])
-
-normedInputs = (inputs - means)/stds
-normedOutputs = (outputs - meanTheta)/stdTheta
-
-post_training_error_evaluation_data = classificationError(DEnet, normedInputs, normedOutputs)*100
+post_training_error_evaluation_data = classificationError(DEnet, evaluation_normedInputs, evaluation_normedOutputs)*100
 
 print "Pre-training error on training data", pre_training_error_training_data, "%"
 print "Post-training error on training data:", post_training_error_training_data, "%"
 print "Post-training error on evaluation data:", post_training_error_evaluation_data, "%"
+
+"Arrays of training and evaluation data errors, respectively:"
+print training_data_errors
+print evaluation_data_errors
+
+DEnet.saveNetwork()
